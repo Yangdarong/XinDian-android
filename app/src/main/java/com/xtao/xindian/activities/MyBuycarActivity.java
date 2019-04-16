4a683348df7aead932a5682d2e164c1316230cec
Yangdarong <yxr800@icloud.com>
2019-04-17 00:08:58 +0800

    不要再失效啊!!!!!!

---
+++ app/src/main/java/com/xtao/xindian/activities/MyBuycarActivity.java
@@ -0,0 +1,15 @@
+package com.xtao.xindian.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xtao.xindian.R;
+
+public class MyBuycarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_buycar);
+    }
+}
