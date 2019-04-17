46ae533a2fd9d7bafa2b7ad3a42dbbd728a2942e
Yangdarong <yxr800@icloud.com>
2019-04-17 18:10:58 +0800

    修改相应布局，添加新的布局页面

---
+++ app/src/main/java/com/xtao/xindian/activities/HomeSearchActivity.java
@@ -0,0 +1,15 @@
+package com.xtao.xindian.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xtao.xindian.R;
+
+public class HomeSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_search);
+    }
+}
